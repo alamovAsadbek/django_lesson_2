47e9cdc6de9688cc82aea21d218b70ad26b617b6
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-19 23:29:08 +0500

    change

--- users/models.py
+++ users/models.py
@@ -2,5 +2,5 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class User(AbstractUser):
+class UserModel(AbstractUser):
     phone_number = models.IntegerField(unique=True)
