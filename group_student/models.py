a51d1f7efa024887f8d18587882206a4ef71c8e2
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-20 00:02:00 +0500

    change

--- group_student/models.py
+++ group_student/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from groups.models import GroupModel
+
 
 class GroupStudentModel(models.Model):
-    pass
+    group_id = models.ForeignKey(GroupModel, on_delete=models.SET_NULL, null=True)
