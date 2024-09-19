e79a70b6d2faee04d46abc4abfb4e50dda6c90be
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-20 00:13:02 +0500

    change

--- users/models.py
+++ users/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class UsersModel(AbstractUser):
+class UsersModel:
     STATUS_CHOICES = (
         ('student', 'student'),
         ('teacher', 'teacher')
