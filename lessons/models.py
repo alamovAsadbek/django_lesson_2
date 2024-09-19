c42517a76cbd1edc24139769810dffd45681dd17
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-19 23:57:09 +0500

    change

--- lessons/models.py
+++ lessons/models.py
@@ -8,3 +8,9 @@ class Lessons(models.Model):
     name = models.CharField(max_length=100)
     group_id = models.ForeignKey(GroupModel, on_delete=models.SET_NULL, null=True)
     teacher_id = models.OneToOneField(UserModel, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Lessons'
+        verbose_name = 'Lesson'
