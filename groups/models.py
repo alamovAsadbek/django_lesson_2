76a0eb11f82ff4028efa8a30394ff63eb8ea2a63
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-19 23:50:30 +0500

    change

--- groups/models.py
+++ groups/models.py
@@ -3,3 +3,9 @@ from django.db import models
 
 class Group(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Group'
+        verbose_name_plural = 'Groups'
