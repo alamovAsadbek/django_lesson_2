417748dc3790775b0d30e414cfc82f8fb8c5df59
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-19 23:58:28 +0500

    cahnge

--- lessons/models.py
+++ lessons/models.py
@@ -4,7 +4,7 @@ from groups.models import GroupModel
 from users.models import UserModel
 
 
-class Lessons(models.Model):
+class LessonsModel(models.Model):
     name = models.CharField(max_length=100)
     group_id = models.ForeignKey(GroupModel, on_delete=models.SET_NULL, null=True)
     teacher_id = models.OneToOneField(UserModel, on_delete=models.SET_NULL, null=True)
