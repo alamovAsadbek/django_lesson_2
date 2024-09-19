7b356e9a2e9f1a9651355a734328424ba123bcfc
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-20 00:14:44 +0500

    change

--- users/models.py
+++ users/models.py
@@ -1,12 +1,13 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class UsersModel:
+class UsersModel(models.Model):
     STATUS_CHOICES = (
         ('student', 'student'),
         ('teacher', 'teacher')
     )
+    first_name = models.CharField(max_length=100, blank=True, null=True)
+    last_name = models.CharField(max_length=100, blank=True, null=True)
     phone_number = models.CharField(max_length=15, unique=True, verbose_name='Phone Number')
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='student', verbose_name='Status')
 
