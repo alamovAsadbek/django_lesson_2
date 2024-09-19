35c7c5d2fbbb234b843f38ca25841041fbec0c12
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-19 23:55:21 +0500

    change

--- lessons/models.py
+++ lessons/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
+from groups.models import GroupModel
+
 
 class Lessons(models.Model):
     name = models.CharField(max_length=100)
-    group_id = models.ForeignKey(GroupModal)
+    group_id = models.ForeignKey(GroupModel, on_delete=models.SET_NULL, null=True)
