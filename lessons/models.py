8c2d193d7cf65002b9f5dbe123ad567731abd14c
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-20 00:04:43 +0500

    change

--- lessons/models.py
+++ lessons/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
-from groups.models import GroupModel
+from groups.models import GroupsModel
 from users.models import UsersModel
 
 
 class LessonsModel(models.Model):
     name = models.CharField(max_length=100)
-    group_id = models.ForeignKey(GroupModel, on_delete=models.SET_NULL, null=True)
+    group_id = models.ForeignKey(GroupsModel, on_delete=models.SET_NULL, null=True)
     teacher_id = models.OneToOneField(UsersModel, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
