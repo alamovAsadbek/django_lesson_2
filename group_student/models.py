d12a164f4f85a1347ae9a4ddd527333ccfd1bba1
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-20 00:04:17 +0500

    change

--- group_student/models.py
+++ group_student/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
 from groups.models import GroupsModel
+from users.models import UsersModel
 
 
 class GroupStudentModel(models.Model):
     group_id = models.ForeignKey(GroupsModel, on_delete=models.SET_NULL, null=True)
+    student_id = models.ForeignKey(UsersModel, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Group Students'
+        verbose_name = 'Group Student'
